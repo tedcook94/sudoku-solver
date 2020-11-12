42380960795fb9618f2260a0b715d54684ebb0df
Ted Cook <33679293+tedcook94@users.noreply.github.com>
2020-11-11 23:23:54 -0600

    Replaces isValid method with recursive attemptSolution method
    attemptSolution finds the potential valid values for the first empty cell and then calls itself again. When a cell runs out of options, it can backtrack to try new solutions.
    
    Also removed the Cell class since the provided flag wasn't need anymore.

--- lib/models/sudoku.dart
+++ lib/models/sudoku.dart
@@ -1,106 +1,16 @@
 import 'dart:math';
 
 class Sudoku {
-  List<List<_Cell>> sudoku = [
-    [
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 6),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 4),
-      new _Cell(provided: false),
-      new _Cell(provided: false)
-    ],
-    [
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 1),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 5),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 9),
-      new _Cell(provided: false)
-    ],
-    [
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 9),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 7),
-      new _Cell(provided: true, number: 4),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false)
-    ],
-    [
-      new _Cell(provided: true, number: 3),
-      new _Cell(provided: true, number: 9),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 6),
-      new _Cell(provided: true, number: 1),
-      new _Cell(provided: true, number: 7)
-    ],
-    [
-      new _Cell(provided: true, number: 4),
-      new _Cell(provided: true, number: 8),
-      new _Cell(provided: true, number: 7),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 6),
-      new _Cell(provided: true, number: 1),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 9)
-    ],
-    [
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 9),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false)
-    ],
-    [
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 4),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 5),
-      new _Cell(provided: true, number: 1),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 3),
-      new _Cell(provided: false)
-    ],
-    [
-      new _Cell(provided: true, number: 6),
-      new _Cell(provided: true, number: 7),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 9),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 2),
-      new _Cell(provided: false)
-    ],
-    [
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 3),
-      new _Cell(provided: true, number: 1),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 2),
-      new _Cell(provided: true, number: 8),
-      new _Cell(provided: false),
-      new _Cell(provided: true, number: 4),
-      new _Cell(provided: false)
-    ]
+  List<List<int>> sudoku = [
+    [0, 4, 0, 0, 0, 0, 6, 2, 0],
+    [1, 0, 2, 8, 6, 7, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 2, 0, 8, 9],
+    [0, 0, 0, 7, 8, 0, 0, 0, 3],
+    [0, 3, 0, 0, 0, 5, 7, 0, 0],
+    [6, 0, 0, 2, 4, 3, 0, 0, 0],
+    [4, 0, 0, 1, 0, 0, 2, 0, 0],
+    [5, 0, 3, 6, 0, 0, 1, 0, 0]
   ];
 
   Sudoku() {
@@ -108,64 +18,93 @@ class Sudoku {
   }
 
   void solve() {
-    if (this._isValid())
-      print("Sudoku is valid!");
-    else
-      print("Sudoku is not valid.");
+    bool success = this._attemptSolution();
+
+    print("Sudoku " + (success ? "has been solved!" : "could not be completed."));
+    print("");
+    this._printSudoku();
   }
 
   void _printSudoku() {
     this.sudoku.forEach((row) {
       String rowConcat = "";
       row.forEach((cell) {
-        rowConcat += cell.number.toString() + " ";
+        rowConcat += cell.toString() + " ";
       });
       print(rowConcat);
     });
   }
 
-  bool _isValid() {
-    // Returns whether or not the sudoku is valid based on current numbers
+  bool _attemptSolution() {
+    // Finds next empty cell and tries to find a valid value for it
+    // If a potentially valid value is found, recursively call the function again for the next cell
+    // When a cell is found with no potential solutions, unwind until you find a previous cell that
+    // has another potential value
+    // If no previous cell contains any more valid values, then the sudoku is invalid
+    // If we reach the end with each cell having a valid value, then the sudoku is solved
+    for (var i = 0; i < pow(this.sudoku.length, 2); i++) {
+      int rowIndex = i ~/ this.sudoku.length;
+      int colIndex = i % this.sudoku.length;
+      int curCell = this.sudoku[rowIndex][colIndex];
 
-    for (var i = 0; i < this.sudoku.length; i++) {
-      List<_Cell> row = this.sudoku[i];
+      // If current cell is a 0, try to find a valid value for it
+      if (curCell == 0) {
+        // Get current row and column as lists
+        List<int> row = this.sudoku[rowIndex];
+        List<int> col = this.sudoku.map((row) => row[colIndex]).toList();
 
-      for (var j = 0; j < row.length; j++) {
-        if (row[j].number == 0) continue;
+        // Get current square as a list of values by finding the appropriate row and column ranges
+        // and flattening into a single list
+        int squareSize = sqrt(this.sudoku.length).round();
+        List<int> square = this
+            .sudoku
+            .getRange(
+                rowIndex - (rowIndex % squareSize), ((rowIndex ~/ squareSize) + 1) * squareSize)
+            .map((row) => row.getRange(
+                colIndex - (colIndex % squareSize), ((colIndex ~/ squareSize) + 1) * squareSize))
+            .expand((cell) => cell)
+            .toList();
 
-        _Cell cell = row[j];
-        List<_Cell> col = this.sudoku.map((row2) => row2[j]).toList();
-        int blockSize = sqrt(this.sudoku.length).round();
+        for (var x = 1; x <= this.sudoku.length; x++) {
+          // Make sure potential value isn't already in current row
+          if (!row.contains(x)) {
+            // Make sure potential value isn't already in current column
+            if (!col.contains(x)) {
+              // Make sure potential value isn't already in current square
+              if (!square.contains(x)) {
+                // Set current cell's value since we haven't found any conflicts
+                this.sudoku[rowIndex][colIndex] = x;
 
-        // See if cell in same row has same number
-        // Only have to look to the right since we're going in order
-        if (-1 != row.indexWhere((cell2) => cell.number == cell2.number, j + 1))
-          return false;
-        // See if cell in same column has same number
-        // Only have to look down since we're going in order
-        if (-1 != col.indexWhere((cell2) => cell.number == cell2.number, i + 1))
-          return false;
-        // See if cell in same block has same number
-        // Only have to look right and down since we're going in order
-        for (int x = i; (x ~/ blockSize) == (i ~/ blockSize); x++) {
-          for (int y = j; (y ~/ blockSize) == (j ~/ blockSize); y++) {
-            if ((x != i || y != j) && this.sudoku[x][y].number == cell.number)
-              return false;
+                // Check if we've finished solving
+                if (this._isComplete())
+                  return true;
+                // If we get true back from our child call, then we've found a solution somewhere
+                // and we bubble it up the recursion chain
+                else if (this._attemptSolution())
+                  return true;
+                // If we get false back, then some cell ran out of options and we need to backtrack
+                // Set the current cell back to 0 so we can try the next possible value for this cell
+                else
+                  this.sudoku[rowIndex][colIndex] = 0;
+              }
+            }
           }
         }
+
+        // If we get here, we went through all possible values for this cell without finding a valid
+        // option, so we return false to indicate that we need to backtrack
+        print("Backtracking from row " + rowIndex.toString() + ", col " + colIndex.toString());
+        return false;
       }
     }
 
-    // If we never find a match, then it must be valid
-    return true;
+    // We should never reach here, because if we reach the last cell we've found the solution
+    // This is here to prevent the compiler from complaining about a potential for no return value
+    return false;
   }
-}
 
-class _Cell {
-  int number;
-  bool provided;
-
-  _Cell({this.number, this.provided}) {
-    if (!this.provided && this.number == null) this.number = 0;
+  bool _isComplete() {
+    // Returns whether or not the sudoku has been completed
+    return !this.sudoku.any((row) => row.any((cell) => cell == 0));
   }
 }
