ece8ab24492603e31a379d6a8b5cf209b7f3827f
Ted Cook <33679293+tedcook94@users.noreply.github.com>
2020-11-18 18:20:30 -0600

    Adds stand-in sudoku table to home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'models/sudoku.dart';
+
+import 'package:sudoku_solver/models/sudoku.dart';
+
+import 'package:sudoku_solver/widgets/sudoku-table.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,13 +31,31 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sudoku Solver'),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        title: Text(
+          'Sudoku Solver',
+          style: TextStyle(color: Colors.grey[600]),
+        ),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: sudoku.solve,
-          child: Text('Solve Sudoku'),
-        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          Expanded(
+            flex: 6,
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 0.0),
+                child: SudokuTable(
+                  tableSize: 9,
+                )),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: RaisedButton(
+              onPressed: sudoku.solve,
+              child: Text('Solve Sudoku'),
+            ),
+          ),
+        ]),
       ),
     );
   }
